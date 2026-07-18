1ae836864e4c306e1d05c360a3da78e162b43d99
KNSN92 <kekenshin92@gmail.com>
2026-07-19 01:35:40 +0900

    feat: mainファイルにtokenizerのテスト用プログラムを一旦記述

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
+use std::io::Write;
+
+use crate::tokenize::Lexer;
+
 mod token;
 mod tokenize;
 
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+    let mut buf = String::new();
+    loop {
+        buf.clear();
+        print!("> ");
+        stdout.flush().unwrap();
+        stdin.read_line(&mut buf).unwrap();
+        let mut lexer = Lexer::new(&buf);
+        while let Some(token) = lexer.tokenize() {
+            println!("{:?}", token);
+        }
+        println!("---");
+    }
 }
